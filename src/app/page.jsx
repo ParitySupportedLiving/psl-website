885b6a1ee6def42f111cf9f3a5643fcaa31c6bef
wSwanepoel199 <wjaco.swanepoel@gmail.com>
2023-07-18 13:46:38 +1000

    added nav and footer, added material-icons font via google fonts

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,9 +1,63 @@
 import React from 'react';
+import Image from 'next/image';
+import PSLImage from '@public/ParitySupportedLiving.png';
 
 const Home = () => {
   return (
-    <div>
+    <div className={`flex min-h-screen flex-col items-center justify-between`} >
+      <nav className={`bg-psl-secondary/80 flex items-center justify-around w-full min-h-5 px-5 py-2`}>
+        <div className={`flex text-sm items-center`}>
+          <span className="material-icons">
+            call
+          </span>
+          <p>0427 358 514</p>
+        </div>
+        <div className={`flex text-sm items-center`}>
+          <span className="material-icons">
+            email
+          </span>
+          <p>contact@paritysl.com</p>
+        </div>
+      </nav>
       <h1>Home</h1>
+      <h2>Test</h2>
+      <footer className={`bg-psl-secondary/80 flex flex-col justify-around w-full p-4`}>
+        <div className={`flex items-center justify-around p-3`}>
+          <div className={`max-w-lg`}>
+            <Image
+              src={PSLImage}
+              alt="parity supported living icon"
+              className='max-h-36 w-auto p-2 mx-auto'
+            />
+            <p className={'py-2'}>At Parity Supported Living, we believe both participants and support workers should work together toward an inclusive and fulfilling life.</p>
+          </div>
+          <div className={` max-w-lg`}>
+            <p>CONTACT DETAILS</p>
+            <ul>
+              <li className={`flex p-2 items-center`}>
+                <span className={`material-icons p-2 rounded-full bg-psl-active-link`}>
+                  call
+                </span>
+                <p >0427 358 514</p>
+              </li>
+              <li className={`flex p-2 items-center`}>
+                <span className={`material-icons p-2 rounded-full bg-psl-active-link`}>
+                  email
+                </span>
+                <p >contact@paritysl.com</p>
+              </li>
+              <li className={`flex p-2 items-center`}>
+                <span className={`material-icons p-2 rounded-full bg-psl-active-link`}>
+                  location_pin
+                </span>
+                <p >Unit 4/116 Mitchell Avenue,
+                  Kurri Kurri. NSW 2327</p>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div className={`text-xs`}>Copyright 2023 © Parity Supported Living | All Rights Reserved</div>
+      </footer>
     </div>
   );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,7 @@
 /** @type {import('tailwindcss').Config} */
+const plugin = require('tailwindcss/plugin');
+const flatten = require('flatten-tailwindcss-theme');
+
 module.exports = {
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
@@ -7,6 +10,14 @@ module.exports = {
   ],
   theme: {
     extend: {
+      colors: {
+        'psl-primary': '#213430',
+        'psl-secondary': '#009368',
+        'psl-primary-text': '#6F7775',
+        'psl-secondary-text': '#DDDDDD',
+        'psl-active-link': '#f37b1c',
+        "psl-active-text": '#F5F5F5',
+      },
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
@@ -14,5 +25,15 @@ module.exports = {
       },
     },
   },
-  plugins: [],
-}
+  plugins: [
+    plugin(({ addUtilities, theme, e, variants }) => {
+      const colors = flatten(theme("colors"));
+      const utilities = Object.entries(colors).reduce((res, [key, value]) => Object.assign(res, {
+        [`.${e(`text-fill-${key}`)}`]: {
+          '-webkit-text-fill-color': `${value}`,
+        }
+      }), {});
+      addUtilities(utilities, variants('text-fill'));
+    }),
+  ],
+};
