3c306fcbe241caf50cd3215c7be0f94789e81821
wSwanepoel199 <wjaco.swanepoel@gmail.com>
2023-07-18 14:21:15 +1000

    finished styling footer and nav, both are fuly responsive

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -5,14 +5,14 @@ import PSLImage from '@public/ParitySupportedLiving.png';
 const Home = () => {
   return (
     <div className={`flex min-h-screen flex-col items-center justify-between`} >
-      <nav className={`bg-psl-secondary/80 flex items-center justify-around w-full min-h-5 px-5 py-2`}>
-        <div className={`flex text-sm items-center`}>
+      <nav className={`bg-psl-secondary/80 sm:flex sm:items-center sm:justify-around w-full min-h-5 px-5 py-2`}>
+        <div className={`flex items-center p-1 sm:p-0`}>
           <span className="material-icons">
             call
           </span>
           <p>0427 358 514</p>
         </div>
-        <div className={`flex text-sm items-center`}>
+        <div className={`flex items-center p-1 sm:p-0`}>
           <span className="material-icons">
             email
           </span>
@@ -21,17 +21,17 @@ const Home = () => {
       </nav>
       <h1>Home</h1>
       <h2>Test</h2>
-      <footer className={`bg-psl-secondary/80 flex flex-col justify-around w-full p-4`}>
-        <div className={`flex items-center justify-around p-3`}>
-          <div className={`max-w-lg`}>
+      <footer className={`bg-psl-secondary/80 flex flex-col justify-around w-full p-4 sm:px-10`}>
+        <div className={`grid grid-cols-1 md:grid-cols-2 gap-4 p-3`}>
+          <div className={`col-span-1 w-full md:w-1/2 m-auto`}>
             <Image
               src={PSLImage}
               alt="parity supported living icon"
-              className='max-h-36 w-auto p-2 mx-auto'
+              className='max-h-36 w-auto p-2 mx-auto md:m-0'
             />
             <p className={'py-2'}>At Parity Supported Living, we believe both participants and support workers should work together toward an inclusive and fulfilling life.</p>
           </div>
-          <div className={` max-w-lg`}>
+          <div className={`col-span-1 w-full md:w-1/2 m-auto`}>
             <p>CONTACT DETAILS</p>
             <ul>
               <li className={`flex p-2 items-center`}>
